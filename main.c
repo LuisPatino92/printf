451091eeb749ae713cd42a7dfd49365a8170b69d
LuisPatino92 <2450@holbertonschool.com>
2020-10-24 21:06:48 +0000

    SOLO MENSAJE

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+
+int main(void)
+{
+	int len, len2;
+
+	len = printf("mensaje de prueba");
+	len2 = _printf("mensaje de prueba");
+
+	printf("original: %d pirata %d", len, len2);
+	
+	return (0);
+}
